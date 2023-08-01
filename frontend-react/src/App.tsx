bf1c65e438c3a76bca8f3d2407f6da8f40e04967
Krzysiek Tokarz <123651137+KrzysztofTokarz@users.noreply.github.com>
2023-08-01 17:55:21 +0200

    setup eslint rules

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -1,19 +1,5 @@
-import { useEffect } from 'react';
-
-function Comp({ aaa }: { aaa: string }) {
-  return <div>{aaa}</div>;
-}
-
 function App() {
-  const aaa = () => {};
-  useEffect(() => {}, [aaa]);
-  return (
-    <>
-      <Comp aaa={'asdsaddas'}></Comp>
-      <Comp aaa={'asdsaddas'}></Comp>
-      <button content={''}></button>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
