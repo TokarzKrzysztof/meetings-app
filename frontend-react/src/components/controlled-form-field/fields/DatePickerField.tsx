63758062934abea0b430cc2315109ca380119a7b
Krzysiek Tokarz <123651137+KrzysztofTokarz@users.noreply.github.com>
2024-02-07 00:17:00 +0100

    datepicker fix

--- frontend-react/src/components/controlled-form-field/fields/DatePickerField.tsx
+++ frontend-react/src/components/controlled-form-field/fields/DatePickerField.tsx
@@ -7,16 +7,18 @@ export type DatePickerFieldProps = DatePickerProps<Dayjs>;
 
 export const DatePickerField = <
   TFieldValues extends FieldValues = FieldValues,
-  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
 >({
   label,
   controller: { field, fieldState },
   ...props
 }: DatePickerFieldProps & ControlledFieldProps<TFieldValues, TName>) => {
+  // this will show empty field on first render, instead of DD.MM.YYYY, and won`t disturb user when changing year
+  const emptyValue = fieldState.isDirty ? dayjs(null) : null;
   return (
     <DatePicker
       label={label}
-      value={field.value ? dayjs(field.value) : null}
+      value={field.value ? dayjs(field.value) : emptyValue}
       inputRef={field.ref}
       slotProps={{
         textField: {
