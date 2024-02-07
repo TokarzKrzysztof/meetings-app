18215e602d8f3579e8139978328ef110cdbc309e
Krzysiek Tokarz <123651137+KrzysztofTokarz@users.noreply.github.com>
2024-02-07 21:43:22 +0100

    fix header behavior on navigation back

--- frontend-react/src/components/Header/Header.tsx
+++ frontend-react/src/components/Header/Header.tsx
@@ -1,20 +1,9 @@
-import useScrollTrigger from '@mui/material/useScrollTrigger';
-import { ReactElement, ReactNode, cloneElement } from 'react';
+import { ReactElement, ReactNode, cloneElement, useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { HeaderMenu } from 'src/components/header/HeaderMenu';
 import { AppBar, Box, Slide, Toolbar } from 'src/ui-components';
 import { AppRoutes } from 'src/utils/enums/app-routes';
 import { PropsWithReactElement } from 'src/utils/types/props';
-
-const HideOnScroll = ({ children }: PropsWithReactElement) => {
-  const trigger = useScrollTrigger();
-
-  return (
-    <Slide appear={false} direction='down' in={!trigger}>
-      {children}
-    </Slide>
-  );
-};
 
 export type HeaderProps = {
   leftSlot?: ReactNode;
@@ -44,5 +33,36 @@ export const Header = ({ leftSlot, secondToolbar }: HeaderProps) => {
       <Toolbar />
       {secondToolbar && cloneElement(secondToolbar, { children: null })}
     </>
+  );
+};
+
+const HideOnScroll = ({ children }: PropsWithReactElement) => {
+  const ignoreRef = useRef(true);
+  const prevScrollYRef = useRef(window.scrollY);
+  const [show, setShow] = useState(true);
+
+  useEffect(() => {
+    // timeout when navigation back happen to ignore first router scroll event
+    setTimeout(() => {
+      ignoreRef.current = false;
+    });
+
+    const handler = () => {
+      if (!ignoreRef.current) {
+        setShow(prevScrollYRef.current > window.scrollY);
+        prevScrollYRef.current = window.scrollY;
+      }
+    };
+
+    window.addEventListener('scroll', handler);
+    return () => {
+      window?.removeEventListener('scroll', handler);
+    };
+  }, []);
+
+  return (
+    <Slide appear={false} direction='down' in={show}>
+      {children}
+    </Slide>
   );
 };
